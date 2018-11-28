0a3e44b5742f571b2f174eddfc81317e81bc7039
Jordan Marques <jordan.marques@canal-plus.com>
2018-11-28 12:16:58 +0100

    add default constructor for Vote

--- src/main/kotlin/fr/jordanmarques/majorityjudgment/model/Vote.kt
+++ src/main/kotlin/fr/jordanmarques/majorityjudgment/model/Vote.kt
@@ -2,5 +2,5 @@ package fr.jordanmarques.majorityjudgment.model
 
 data class Vote(
         var choice: Choice = Choice(""),
-        var appreciation: Appreciation
+        var appreciation: Appreciation = Appreciation.VERY_GOOD
 )
